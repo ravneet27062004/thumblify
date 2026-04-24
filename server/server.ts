d092069a5596a31c66b572ff31dfd59d210a06b2
ravneet kaur <146877258+ravneet27062004@users.noreply.github.com>
2026-04-24 14:25:58 +0530

    cookies changes

--- server/server.ts
+++ server/server.ts
@@ -34,6 +34,8 @@ app.use(cors({
     origin:['http://localhost:5173', 'https://localhost:3000',"https://thumblify-nu.vercel.app"],
     credentials: true
 }))
+
+app.set('trust proxy',1);
 app.use(session({
     secret: process.env.SESSION_SECRET as string,
     resave: false,
@@ -42,7 +44,7 @@ app.use(session({
       maxAge: 1000 * 60 * 60 * 24*7,
     httpOnly:true,
     secure:process.env.NODE_ENV=='production',
-    sameSite:'none',
+    sameSite:process.env.NODE_ENV=='production'?'none':'lax',
     path:'/'
   } ,
     store:MongoStore.create({
